98bb9ec52b28dd4e2780f1f7a27ba7967fe85693
Anand997758 <96283755+Anand997758@users.noreply.github.com>
2022-11-28 07:24:24 +0530

    this is my first commit

---
+++ operators.py
@@ -0,0 +1,109 @@
+#arthamatic operator
+a = 7
+b = 10
+print("addition: ", a+b) #17
+print("subcration:", b-a) #3
+print("multiplication: ", a*b) #70
+print("division: ", b/a) #3
+print("module: ",a%b) #7
+print("power: ", a**b) #282475249
+
+#assignment operator
+a = 10
+b = 15
+a=10+20
+print("assignment:", type(a))
+a+=b
+print(a)
+b=+a
+print(type(a))
+b-=a
+print(type(b))
+a==b
+print(a)
+a-= 3
+print(type(a))
+d = 4
+d *= 4
+print(d)
+e = 40
+e/= 10
+print(e,type(e))
+f=-6-6
+print(f,type(f))
+g= +12-6
+print(g,type(g))
+h=5
+h **=4
+print(h)
+
+#comparison operator
+a = 7
+b = 6
+print(a==b)
+c = 8
+d = 8
+print(c==d)
+
+e = 10
+f = 11
+print(e!=f)
+
+g = 11
+h = 11
+print(g!=h)
+
+a = 5
+
+b = 2
+
+# equal to operator
+print('a == b =', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
+
+# logical AND
+print(True and True)     # True
+print(True and False)    # False
+
+# logical OR
+print(True or False)     # True
+
+# logical NOT
+print(not True)          # False
+
+message = 'Hello world'
+dict1 = {1:'a', 2:'b'}
+
+# check if 'H' is present in message string
+print('H' in x)  # prints True
+
+# check if 'hello' is present in message string
+print('hello' not in x)  # prints True
+
+# check if '1' key is present in dict1
+print(1 in y)  # prints True
+
+# check if 'a' key is present in dict1
+print('a' in y)  # prints False
+
+
+
+
+
+
+
+
---
+++ values.py
@@ -0,0 +1,28 @@
+a = 10
+print(a)
+b = 10+20
+print(b)
+c = -10+20
+print(c)
+d = 10*40
+print(d)
+e = 30-40+20
+print(e)
+f = 20*50-40
+print(f)
+g = 70-50+20
+print(g)
+h = 90+20+60
+print(h)
+i = 30-50-20
+print(i)
+j = 20
+print(j)
+k = 20.5
+print(k)
+l = 20+20.5
+print(l)
+m = 50+2-60.5
+print(m)
+n = 80-6.2
+print(n)
